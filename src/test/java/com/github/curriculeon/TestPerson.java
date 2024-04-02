7c333cf28407ed2c313bba901062cb2a0051e1c6
Brent <BrentC0328@gmail.com>
2024-04-02 18:08:01 -0400

    Person test, learner interface

--- src/test/java/com/github/curriculeon/TestPerson.java
+++ src/test/java/com/github/curriculeon/TestPerson.java
@@ -1,5 +1,38 @@
 package com.github.curriculeon;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
+
+    @Test
+    public void testPersonConstructor(){
+        //Given
+        Person person = new Person(1L, "Bob");
+        String expectedName = "Bob";
+        Long expectedId = 1L;
+
+        //When
+        String actualName = person.getName();
+        Long actualId = person.getId();
+
+        //Then
+        Assert.assertEquals(expectedId, actualId);
+        Assert.assertEquals(expectedName, actualName);
+    }
+
+    @Test
+    public void testSetName(){
+        //Given
+        Person person = new Person(1L, "Bob");
+        String expectedName = "Pop";
+
+        //When
+        person.setName("Pop");
+        String actualName = person.getName();
+
+        //Then
+        Assert.assertEquals(expectedName, actualName);
+    }
 
 }
