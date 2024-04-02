0be549b117bfc4c51fc018430b53301b8d6c210c
Brent <BrentC0328@gmail.com>
2024-04-02 17:55:04 -0400

    Person class

--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -2,4 +2,27 @@ package com.github.curriculeon;
 
 public class Person {
 
+    private final Long id;
+
+    private String name;
+
+    public Person(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public Long getId(){
+        return this.id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+
+
 }
